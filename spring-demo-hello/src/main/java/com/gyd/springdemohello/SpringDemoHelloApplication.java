51f98ca1191c9e9a4254eedb6505ee9a9d751c96
guoyading <964781872@qq.com>
2022-05-28 17:28:56 +0800

    spring demo init

---
+++ spring-demo-hello/src/main/java/com/gyd/springdemohello/SpringDemoHelloApplication.java
@@ -0,0 +1,13 @@
+package com.gyd.springdemohello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoHelloApplication.class, args);
+	}
+
+}
---
+++ spring-demo-hello/src/test/java/com/gyd/springdemohello/SpringDemoHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyd.springdemohello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
