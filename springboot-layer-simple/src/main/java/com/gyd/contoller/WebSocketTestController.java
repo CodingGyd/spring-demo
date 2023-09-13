2628fc8d77e48dd031db9440d6da7611919c81fb
郭雅定 <guoyading@wondersgroup.com>
2023-09-13 10:33:06 +0800

    websocket

---
+++ springboot-layer-simple/src/main/java/com/gyd/contoller/WebSocketTestController.java
@@ -0,0 +1,44 @@
+package com.gyd.contoller;
+
+
+import com.gyd.websocket.WebSocketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
+
+
+/*
+ *@Controller：如果当前类所在的包配置了Spring容器包扫描，具有
+ *该注解的类，就会作为bean注册到spring容器中，由spring容器创建实例。
+ */
+@Controller
+@RequestMapping("/show/")
+public class WebSocketTestController {
+
+    @Autowired
+    private WebSocketService webSocketService;
+
+    /**
+     * 跳转thymeleaf模板路径
+     *
+     * @return
+     */
+    @RequestMapping("/topic")
+    public String websocket() {
+        return "topic";
+    }
+
+    /**
+     * 模拟创建订单，发送消息到客户端
+     *
+     * @return
+     */
+    @RequestMapping("/createOrder")
+    public @ResponseBody String createOrder() {
+        webSocketService.sendMessage("你有新的订单，请及时处理========>" + UUID.randomUUID());
+        return "新增订单成功!";
+    }
+}
---
+++ springboot-layer-simple/src/main/java/com/gyd/websocket/WebSocketService.java
@@ -0,0 +1,62 @@
+package com.gyd.websocket;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Component
+@ServerEndpoint("/websocket")
+
+public class WebSocketService {
+    private Session session;
+
+    //保存连接
+    private static CopyOnWriteArraySet<WebSocketService> webSocketService = new CopyOnWriteArraySet<>();
+
+    /**
+     * 建立连接
+     * @param session
+     */
+    @OnOpen
+    public void opOpen(Session session) {
+        this.session = session;
+        webSocketService.add(this);
+        System.out.println("有新的连接=============》" + webSocketService.size());
+    }
+
+    /**
+     * 断开连接
+     */
+    @OnClose
+    public void onClose() {
+        webSocketService.remove(this);
+        System.out.println("断开连接=============》" + webSocketService.size());
+    }
+
+    /**
+     * 接收客户端消息
+     * @param message
+     */
+    @OnMessage
+    public void onMessage(String message) {
+        System.out.println("收到客户端消息" + message);
+    }
+
+    /**
+     * 发送消息到客户端
+     * @param message
+     */
+    public void sendMessage(String message) {
+        for (WebSocketService webSocketService2 : webSocketService) {
+            System.out.println("广播消息" + message);
+            webSocketService2.session.getAsyncRemote().sendText(message);
+        }
+    }
+
+}
