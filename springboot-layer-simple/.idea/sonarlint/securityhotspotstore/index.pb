d525f1201618ec0ad7c674c9c5652cf1ad65fc09
郭雅定 <guoyading@wondersgroup.com>
2023-09-13 09:34:04 +0800

    引入web页面支持

---
+++ springboot-layer-simple/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+`
+0src/main/java/com/gyd/constant/XXXConstants.java,e\3\e3b9f636997adc22ab4c190e44a7d3267edb7848
+^
+.src/main/java/com/gyd/service/UserService.java,9\f\9f8e6a67c31e26c38f244a368ffc347557678196
---
+++ springboot-layer-simple/src/main/java/com/gyd/contoller/PageTestController.java
@@ -0,0 +1,31 @@
+package com.gyd.contoller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ *@Controller：如果当前类所在的包配置了Spring容器包扫描，具有
+ *该注解的类，就会作为bean注册到spring容器中，由spring容器创建实例。
+ */
+@Controller
+@RequestMapping("/show/")
+public class PageTestController {
+
+    @RequestMapping("findAll")
+    public String findAll(Model model) {
+        List<String> lists = loadDbData();
+        model.addAttribute("list", lists);
+        return "main";
+
+    }
+
+    private List<String> loadDbData(){
+        //模拟查询db
+        return Arrays.asList("1","2","3");
+    }
+}
